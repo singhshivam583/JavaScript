d0aa0fdc0f73212be10c484eaf2a9514dc613f71
Shvam Singh <singhshivam583@yahoo.in>
2023-08-27 18:13:20 +0530

    update

---
+++ 05_itrattions/one.js
@@ -0,0 +1,25 @@
+// for loop
+for (let i = 0; i < 10; i++) {
+    const element = i; 
+    if (element==5){
+        // console.log("I am the fifth number");
+    }
+    // console.log(element); 
+}
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`Table of${i}:`)
+    for (let j = 1; j <=10; j++) {
+        // console.log(`Inner Loop Value ${j} and outer loop value ${i}`);
+        // console.log(i, '*', j, '=', i*j);
+    }
+    
+}
+
+let myArr=["flash", "batman", "superman"]
+console.log(myArr.length);
+for (let i = 0; i < myArr.length; i++) {
+    const element = myArr[i];
+    console.log(element);
+
+}
