0479d0f1b1c40f0aa08fd775659036327f6b05ab
Shvam Singh <singhshivam583@yahoo.in>
2023-08-27 18:49:33 +0530

    update

---
+++ 05_itrattions/two.js
@@ -0,0 +1,19 @@
+
+index=0
+while (index<=10){
+    // console.log(`Value of index is ${index}`);
+    index=index+2
+}
+
+let myArr=["batman","flash","superman"]
+let arr=0
+while(arr<myArr.length){
+    // console.log(`Value is ${myArr[arr]}`);
+    arr ++
+}
+
+let score=15
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}while(score<=10);
