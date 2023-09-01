c2f4d7c0a71742058d3b3d4e15385c09eb97024b
Shvam Singh <singhshivam583@yahoo.in>
2023-09-01 15:16:38 +0530

    update

--- 07_DOM_Project/5-keyboard/chaiaurcode.js
+++ 07_DOM_Project/5-keyboard/chaiaurcode.js
@@ -1,1 +1,22 @@
-console.log('Project 5');
+
+const insert=document.getElementById('insert')
+
+window.addEventListener('keypress',(e)=>{
+    insert.innerHTML=`
+        <div>Result:</div>
+        <div class='color'>
+            <table>
+                <tr>
+                  <th>Key</th>
+                  <th>KeyCode</th>
+                  <th>Code</th>
+                </tr>
+                <tr>
+                  <td>${e.key===" " ? "Spacebar" : e.key }</td>
+                  <td>${e.keyCode}</td>
+                  <td>${e.code}</td>
+                </tr>
+            </table>
+        </div>    
+    `
+})
