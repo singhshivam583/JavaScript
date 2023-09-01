cdff65b7255bccdc07e8ac17f485111889403ac2
Shvam Singh <singhshivam583@yahoo.in>
2023-09-01 14:18:57 +0530

    update

---
+++ 07_DOM_Project/6-unlimitedColors/chaiaurcode.js
@@ -0,0 +1,32 @@
+// Generate Random Color
+
+const randomColor=function(){
+    const hex='0123456789ABCDEF';
+    let color="#";
+    
+    for (let i = 0; i < 6; i++) {
+        color +=hex[Math.floor(Math.random()*16)]
+    }
+    return color;
+};
+
+// console.log(randomColor());
+let intervalId;
+
+const startChangingColor=function(){ 
+    const changeBgColor=function(){
+        document.body.style.backgroundColor=randomColor();
+    }  
+    intervalId= setInterval(changeBgColor,1000)
+}
+
+const stopChangingColor=function(){
+     clearInterval(intervalId);
+     intervalId=null;               
+}
+
+document.querySelector('#start').
+addEventListener('click',startChangingColor)
+
+document.querySelector('#stop').
+addEventListener('click',stopChangingColor)
